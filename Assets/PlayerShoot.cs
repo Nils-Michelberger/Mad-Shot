f8f5406666530722357193e7030ea896254b3604
Nils Michelberger <nmichelb@stud.hs-heilbronn.de>
2021-04-05 17:56:20 +0200

    Implemented shooting

--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviourPunCallbacks
 {
-    public Weapon weapon;
     public Camera cam;
     public LayerMask mask;
-    
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem muzzleFlashFPS;
+    public GameObject impactEffect;
+    public AudioSource fireSound;
+
+    public float fireRate = 15f;
+    public float range = 200f;
+    public float damage = 10f;
+
+    private float nextTimeToFire = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +27,27 @@ public class PlayerShoot : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            
+            muzzleFlash.Play();
+            muzzleFlashFPS.Play();
+            fireSound.Play();
+            
             RaycastHit hit;
 
-            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range, mask))
+            if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range, mask))
             {
                 Debug.Log(hit.collider);
+
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+
+                Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             }
         }
     }
---
+++ Assets/Target.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
