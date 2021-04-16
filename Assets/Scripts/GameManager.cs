426b5f1dec3424d19b0414a0e9a0b10578f4bc33
Nils Michelberger <nmichelb@stud.hs-heilbronn.de>
2021-04-16 19:35:55 +0200

    Minor refactoring

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,7 +35,6 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
             // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-
             PhotonNetwork.Instantiate(playerPrefab.name, spawnpoints[random.Next(0, spawnpoints.Length)], Quaternion.identity);
         }
         else
